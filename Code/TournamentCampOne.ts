b9471fe2d4fd23e0403fa5fe8de4af5b25c3a0c5
Nikola <glamdring0@hotmail.com>
2018-02-25 21:27:45 +0100

    Tournament1

---
+++ Code/TournamentCampOne.ts
@@ -0,0 +1,74 @@
+export { ForestTrailOne };
+
+import Engineer from "./Engineer";
+import { Player } from "./Player";
+import { Movement } from "./Movement";
+import { ParticleSystemMaterial, Vertex } from "three";
+import { Dialog } from "./Dialog";
+import { Curio } from "./Curio";
+import { GameScene } from "./GameScene";
+
+import { Rabbit } from "./Characters/Rabbit";
+import { Transition } from "./Transition";
+
+class ForestTrailOne extends GameScene
+{
+    private _Rabbit:Rabbit;
+    private _Signpost:Transition;
+    public get Pause():boolean { return this._Pause; }
+    public set Pause(value:boolean) { this._Pause = value; }
+    public get Player():Player { return this._Player; }
+    public set Player(value:Player) { this._Player = value; }
+
+    public constructor()
+    {
+        super();
+        this.Name = "ForestTrailOne";
+        this.Init();
+    }
+    public Init(): void
+    {
+        this._Curio = [];
+        this._Movement = new Movement(this);
+
+        this._Player = new Player(this);
+        this._Player.Trans.Scale = new Engineer.Vertex(279, 333, 1);
+        this._Player.Trans.Translation = new Engineer.Vertex(200, 780, 1);
+
+        this._Dialog = new Dialog();
+
+        this._Rabbit = new Rabbit();
+        this._Rabbit.Init(new Engineer.Vertex(5100, 680, 0.8), new Engineer.Vertex(300, 300, 1));
+
+        this._Signpost = new Transition(null, "Signpost", "Pavillion1", "Lower Tournament Camp");
+        this._Signpost.Init(new Engineer.Vertex(5500, 680, 0.7));
+        
+        this.BackColor = Engineer.Color.FromRGBA(0, 0, 0, 255);
+        this.GenerateBackground();
+        this.AddSceneObject(this._Rabbit);
+        this.AddSceneObject(this._Player);
+        this.AddSceneObject(this._Signpost);
+    }
+    private KeyPress(G: any, Args: any): void
+    {
+        if(this._Pause) return;
+        // Key Code here
+    }
+    private SceneUpdate() : void
+    {
+        if(this._Pause) return;
+        // Update Code here
+    }
+    private GenerateBackground() : void
+    {
+        let Backs:Engineer.ImageCollection = new Engineer.ImageCollection(null, ["Resources/Textures/ForestTrailOne.png"]);
+        let Back:Engineer.Tile = new Engineer.Tile();
+        Back.Name = "Back";
+        Back.Collection = Backs;
+        Back.Index = 0;
+        Back.Trans.Scale = new Engineer.Vertex(5760, 1080, 1);
+        Back.Trans.Translation = new Engineer.Vertex(2800, 540, 0);
+        this._Background = Back;
+        this.AddSceneObject(this._Background);
+    }
+}
