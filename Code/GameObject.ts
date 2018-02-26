fb2fbdf3914298289039663bc458c78e944030c9
Nikola <glamdring0@hotmail.com>
2018-02-26 01:04:56 +0100

    DialoguesInput

--- Code/GameObject.ts
+++ Code/GameObject.ts
@@ -9,33 +9,224 @@ let DefaultGameObject:any =
             Chats:
             [
                 {
-                    Text: "To Rabbit",
+                    Text: "To Squirrel",
                     Options:
                     [
                         {
-                            Text: "Good morning lady!",
-                            Locked: false,
-                            Link: 1
-                        },
-                        {
-                            Text: "Good morning gentleman!",
+                            Text: "...",
                             Locked: false,
                             Link: 1
                         }
                     ]
                 },
                 {
-                    Text: "Did you just assume my gender?!",
+                    Text: "To Rabbit",
                     Options:
                     [
                         {
-                            Text: "Goodbye.",
+                            Text: "How do you do, kind sir",
+                            Locked: false,
+                            Link: 3
+                        },
+                        {
+                            Text: "Greetings, m'lady!",
+                            Locked: false,
+                            Link: 3
+                        }
+                    ]
+                },
+                {
+                    Text: "To Squirrel",
+                    Options:
+                    [
+                        {
+                            Text: "Did you just assume my gender?!.",
+                            Locked: false,
+                            Link: 4
+                        }
+                    ]
+                },
+                {
+                    Text: "To Rabbit",
+                    Options:
+                    [
+                        {
+                            Text: "Uhh... sorry.",
                             Locked: false,
                             Link: -1
                         }
                     ]
                 }
             ]
-        }
+        },
+        {
+            Name:"Raccoon",
+            Chats:
+            [
+                {
+                    Text: "To Squirrel",
+                    Options:
+                    [
+                        {
+                            Text: "Top of the morning, good sir![Rubbing his hands nervously].",
+                            Locked: false,
+                            Link: 1
+                        }
+                    ]
+                },
+                {
+                    Text: "To Raccoon",
+                    Options:
+                    [
+                        {
+                            Text: "My good man, you do seem concerned. What worries you this fine day, may i ask?.",
+                            Locked: false,
+                            Link: 2
+                        },
+                        {
+                            Text: "Out of my way scoundrel.",
+                            Locked: false,
+                            Link: 3
+                        }
+                    ]
+                },
+                {
+                    Text: "To Squirrel",
+                    Options:
+                    [
+                        {
+                            Text: "A large wager lost i am afraid... and with the sum at lost my chance to purchase my freedom. But Theft! -I'd never. 'Tis mighty refreshing not to be judged upon. I wish you the best of luck, good sir.",
+                            Locked: false,
+                            Link: 4
+                        }                       
+                    ]                   
+                },
+                {
+                    Text: "To Squirrel",
+                    Options:
+                    [
+                        {
+                            Text: "You'd better watch your tongue, stranger... you may come to find that knights aren't all that safe in these woods...[Turns away]",
+                            Locked: false,
+                            Link: 5
+                        }
+                    ]
+                },
+                {
+                    Text: "To Raccoon",
+                    Options:
+                    [
+                        {
+                            Text: "Good luck!",
+                            Locked: false,
+                            Link: -1
+                        }
+                    ]
+                },
+                {
+                    Text: "To Raccoon",
+                    Options:
+                    [
+                        {
+                            Text: "...",
+                            Locked: false,
+                            Link: -1
+                        }     
+                    ]
+                }
+            ]
+        },
+        {
+            Name:"DeadWolf",
+            Chats:
+            [
+                {
+                    Text: "Squirrel",
+                    Options:
+                    [
+                        {
+                            Text: "A body of an old canine lies on the grass on its stomach. He is wearing a fancy plate armor, which looks very shiny and festive. His cape looks quite expensive.",
+                            Locked: false,
+                            Link: 1
+                        }
+                    ]
+                },
+                {
+                    Text: "Squirrel",
+                    Options:
+                    [
+                        {
+                            Text: "Turn him around",
+                            Locked: false,
+                            Link: 2
+                        },
+                        {
+                            Text: "Leave",
+                            Locked: false,
+                            Link: -1
+                        }
+                    ]
+                },
+                {
+                    Text: "Squirrel",
+                    Options:
+                    [
+                        {
+                            Text: "Examine face",
+                            Locked: false,
+                            Link: 3
+                        },
+                        {
+                            Text: "Examine arms",
+                            Locked: false,
+                            Link: 4
+                        },
+                        {
+                            Text: "Examine torso",
+                            Locked: false,
+                            Link: 5
+                        },
+                        {
+                            Text: "Leave",
+                            Locked: false,
+                            Link: -1
+                        }
+                    ]
+                },
+                {
+                    Text: "Squirrel",
+                    Options:
+                    [
+                        {
+                            Text: "He is middle aged wolf with sharp fangs the look in his eyes is surprised. He still reaks of alcohol.",
+                            Locked: false,
+                            Link: 2
+                        }
+                    ]
+                },
+                {
+                    Text: "Squirrel",
+                    Options:
+                    [
+                        {
+                            Text: "There is mix of blood and skin under his clwas, he wa a string wolf",
+                            Locked: false,
+                            Link: 2
+                        }
+                    ]
+                },
+                {
+                    Text: "Squirrel",
+                    Options:
+                    [
+                        {
+                            Text: "You see two puncture wounds between plates under his armpit.",
+                            Locked: false,
+                            Link: 2
+                        }
+                    ]
+                }
+            ]
+        }        
     ]
 };
