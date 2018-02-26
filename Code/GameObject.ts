a2633540798e4e81e5685d0aaa89f5623ecc2190
Nikola <glamdring0@hotmail.com>
2018-02-26 03:48:12 +0100

    Dialogs fix

--- Code/GameObject.ts
+++ Code/GameObject.ts
@@ -9,45 +9,23 @@ let DefaultGameObject:any =
             Chats:
             [
                 {
-                    Text: "To Squirrel",
+                    Text: "...",
                     Options:
                     [
                         {
-                            Text: "...",
+                            Text: "How do you do, kind sir",
+                            Locked: false,
+                            Link: 1
+                        },
+                        {
+                            Text: "Greetings, m'lady!",
                             Locked: false,
                             Link: 1
                         }
                     ]
                 },
                 {
-                    Text: "To Rabbit",
-                    Options:
-                    [
-                        {
-                            Text: "How do you do, kind sir",
-                            Locked: false,
-                            Link: 3
-                        },
-                        {
-                            Text: "Greetings, m'lady!",
-                            Locked: false,
-                            Link: 3
-                        }
-                    ]
-                },
-                {
-                    Text: "To Squirrel",
-                    Options:
-                    [
-                        {
-                            Text: "Did you just assume my gender?!.",
-                            Locked: false,
-                            Link: 4
-                        }
-                    ]
-                },
-                {
-                    Text: "To Rabbit",
+                    Text: "Did you just assume my gender?!.",
                     Options:
                     [
                         {
@@ -64,56 +42,23 @@ let DefaultGameObject:any =
             Chats:
             [
                 {
-                    Text: "To Squirrel",
-                    Options:
-                    [
-                        {
-                            Text: "Top of the morning, good sir![Rubbing his hands nervously].",
-                            Locked: false,
-                            Link: 1
-                        }
-                    ]
-                },
-                {
-                    Text: "To Raccoon",
+                    Text: "Top of the morning, good sir![Rubbing his hands nervously].",
                     Options:
                     [
                         {
                             Text: "My good man, you do seem concerned. What worries you this fine day, may i ask?.",
                             Locked: false,
-                            Link: 2
+                            Link: 1
                         },
                         {
                             Text: "Out of my way scoundrel.",
                             Locked: false,
-                            Link: 3
+                            Link: 2
                         }
                     ]
                 },
                 {
-                    Text: "To Squirrel",
-                    Options:
-                    [
-                        {
-                            Text: "A large wager lost i am afraid... and with the sum at lost my chance to purchase my freedom. But Theft! -I'd never. 'Tis mighty refreshing not to be judged upon. I wish you the best of luck, good sir.",
-                            Locked: false,
-                            Link: 4
-                        }                       
-                    ]                   
-                },
-                {
-                    Text: "To Squirrel",
-                    Options:
-                    [
-                        {
-                            Text: "You'd better watch your tongue, stranger... you may come to find that knights aren't all that safe in these woods...[Turns away]",
-                            Locked: false,
-                            Link: 5
-                        }
-                    ]
-                },
-                {
-                    Text: "To Raccoon",
+                    Text: "A large wager lost i am afraid... and with the sum at lost my chance to purchase my freedom. But Theft! -I'd never. 'Tis mighty refreshing not to be judged upon. I wish you the best of luck, good sir.",
                     Options:
                     [
                         {
@@ -124,14 +69,14 @@ let DefaultGameObject:any =
                     ]
                 },
                 {
-                    Text: "To Raccoon",
+                    Text: "You'd better watch your tongue, stranger... you may come to find that knights aren't all that safe in these woods...[Turns away]",
                     Options:
                     [
                         {
                             Text: "...",
                             Locked: false,
                             Link: -1
-                        }     
+                        }
                     ]
                 }
             ]
@@ -141,24 +86,13 @@ let DefaultGameObject:any =
             Chats:
             [
                 {
-                    Text: "Squirrel",
-                    Options:
-                    [
-                        {
-                            Text: "A body of an old canine lies on the grass on its stomach. He is wearing a fancy plate armor, which looks very shiny and festive. His cape looks quite expensive.",
-                            Locked: false,
-                            Link: 1
-                        }
-                    ]
-                },
-                {
-                    Text: "Squirrel",
+                    Text: "A body of an old canine lies on the grass on its stomach. He is wearing a fancy plate armor, which looks very shiny and festive. His cape looks quite expensive.",
                     Options:
                     [
                         {
                             Text: "Turn him around",
                             Locked: false,
-                            Link: 2
+                            Link: 1
                         },
                         {
                             Text: "Leave",
@@ -166,9 +100,9 @@ let DefaultGameObject:any =
                             Link: -1
                         }
                     ]
-                },
+                },                
                 {
-                    Text: "Squirrel",
+                    Text: "You can see he probably died of puncture wounds. He still reaks of alcohol.",
                     Options:
                     [
                         {
@@ -194,35 +128,52 @@ let DefaultGameObject:any =
                     ]
                 },
                 {
-                    Text: "Squirrel",
+                    Text: "He is middle aged wolf with sharp fangs the look in his eyes is surprised. He still reaks of alcohol.",
                     Options:
                     [
                         {
-                            Text: "He is middle aged wolf with sharp fangs the look in his eyes is surprised. He still reaks of alcohol.",
+                            Text: "Continue examining",
                             Locked: false,
                             Link: 2
                         }
                     ]
                 },
                 {
-                    Text: "Squirrel",
+                    Text: "There is mix of blood and skin under his clwas, he wa a string wolf",
                     Options:
                     [
                         {
-                            Text: "There is mix of blood and skin under his clwas, he wa a string wolf",
+                            Text: "Continue examining",
                             Locked: false,
                             Link: 2
                         }
                     ]
                 },
                 {
-                    Text: "Squirrel",
+                    Text: "You see two puncture wounds between plates under his armpit.",
                     Options:
                     [
                         {
-                            Text: "You see two puncture wounds between plates under his armpit.",
+                            Text: "Continue examining",
                             Locked: false,
                             Link: 2
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            Name:"Fox",
+            Chats:
+            [
+                {
+                    Text: "Sorry, can't talk. Now... where is my lucky arrow?",
+                    Options:
+                    [
+                        {
+                            Text: "...",
+                            Locked: false,
+                            Link: -1
                         }
                     ]
                 }
